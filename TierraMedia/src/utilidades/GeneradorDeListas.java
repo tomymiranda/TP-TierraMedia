535f797651287945db7f6f25184891e25d831ca2
Lucas <lucas.rojas@live.com.ar>
2019-05-15 00:39:47 -0300

    Fix generadorlistas

--- TierraMedia/src/utilidades/GeneradorDeListas.java
+++ TierraMedia/src/utilidades/GeneradorDeListas.java
@@ -1,16 +1,12 @@
 package utilidades;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
-import componentes.Atraccion;
-import componentes.Usuario;
+import java.util.*;
+import componentes.*;
 
 public class GeneradorDeListas {
 	
 	public List<Atraccion> armarPosiblesAtraccionesParaUsuario(Usuario usuario, List<Atraccion> listadoAtracciones) {
-			
+
 			Comparator<Atraccion> comparador = new Comparator<Atraccion>() {
 				@Override
 				public int compare(Atraccion a1, Atraccion a2) {
@@ -23,8 +19,8 @@ public class GeneradorDeListas {
 			
 			Collections.sort(listadoAtracciones, comparador);
 			
-			listadoAtracciones.removeIf(item -> item.getCosto() >= usuario.getCantidadDeMonedas() || 
-					item.getTiempoDeDuracion() >= usuario.getTiempoDisponible() ||
+			listadoAtracciones.removeIf(item -> item.getCosto() > usuario.getCantidadDeMonedas() || 
+					item.getTiempoDeDuracion() > usuario.getTiempoDisponible() ||
 					usuario.getListaAtracciones().contains(item) ||
 					item.getcapacidadRestante() == 0);
 			
