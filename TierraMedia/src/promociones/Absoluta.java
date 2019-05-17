739ff142e6c7f498036699bc2c9a2d49d6a13300
Lucas <lucas.rojas@live.com.ar>
2019-05-17 02:02:33 -0300

    Agrego resto de promociones

--- TierraMedia/src/promociones/Absoluta.java
+++ TierraMedia/src/promociones/Absoluta.java
@@ -1,6 +1,5 @@
 package promociones;
 
-import java.util.Calendar;
 import java.util.List;
 
 import componentes.Atraccion;
@@ -8,12 +7,16 @@ import componentes.Promocion;
 
 public class Absoluta extends Promocion {
 
-	public Absoluta(String nombre, int costo, List<Atraccion> atracciones, String descripcion) {
+	private int costo = 0;
+
+	public Absoluta(String nombre, List<Atraccion> atracciones, int costoAbsoluto, String descripcion) {
 		super(nombre, atracciones, descripcion);
+
+		this.costo = costoAbsoluto;
+		super.setCosto(costo);
 	}
 
-	public void aplicarDescuento() {
-
+	public int obtenerCosto() {
+		return costo;
 	}
-
 }
--- TierraMedia/src/promociones/AxB.java
+++ TierraMedia/src/promociones/AxB.java
@@ -7,12 +7,33 @@ import componentes.Promocion;
 
 public class AxB extends Promocion {
 
-	public AxB(String nombre, List<Atraccion> atracciones, String descripcion) {
+	private List<Atraccion> atracciones;
+	private int costo = 0;
+	private int costoDePromo;
+	private String atraccionArestar;
+
+	public AxB(String nombre, List<Atraccion> atracciones, String atraccionArestar, String descripcion) {
 		super(nombre, atracciones, descripcion);
+
+		this.atracciones = atracciones;
+		this.atraccionArestar = atraccionArestar;
+		calcularCosto();
+		super.setCosto(costo);
 	}
 
-	public void aplicarDescuento() {
+	private void calcularCosto() {
+		for (Atraccion atr : atracciones) {
+			if (atr.getNombre() != atraccionArestar)
+				costo += atr.getCosto();
+		}
+	}
 
+	public String obtenerAtraccionArestar() {
+		return atraccionArestar;
+	}
+
+	public int obtenerCosto() {
+		return costo;
 	}
 
 }
