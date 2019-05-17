5f6967a7261796d6c5a90ad36da3cd1e884d131c
killz <killz@DESKTOP-6VTLRDB>
2019-05-17 02:28:55 -0300

    "Agrego errores"

--- TierraMedia/src/principal/Menu.java
+++ TierraMedia/src/principal/Menu.java
@@ -1,4 +1,5 @@
 package principal;
+
 import utilidades.*;
 
 import java.io.*;
@@ -8,123 +9,124 @@ import componentes.*;
 
 public class Menu {
 
-	private List<Atraccion> listaAtraccionesUsuario; 
+	private List<Atraccion> listaAtraccionesUsuario;
 	private List<Atraccion> atraccionesParaUsuario;
 	List<Atraccion> listaAtraccionesGeneral;
-	
+
 	private List<Promocion> listaPromocionesGeneral;
 	List<Promocion> listaPromocionesParaUsuario;
 	private List<Promocion> promocionesParaUsuario;
-	
+
 	private List<Usuario> listaUsuarios;
 
 	private int opcionSeleccionada;
 	Visualizador view = new Visualizador();
 	Archivo archivo = new Archivo();
 	GeneradorDeListas generador = new GeneradorDeListas();
-	
+
 	public void ejecutar() {
 		leerArchivos();
-		
+
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		if(listaUsuarios.size() > 0 && listaAtraccionesGeneral.size() > 0) {
+		if (listaUsuarios.size() > 0 && listaAtraccionesGeneral.size() > 0) {
+
 			
-		try {
-			System.out.println("Usuarios size" + listaUsuarios.size());
-			for(Usuario usuario : listaUsuarios) {
-				// Se clonan para no referenciar al original y no borrar elementos del original
-				listaAtraccionesUsuario = new ArrayList<Atraccion>();
-				listaAtraccionesUsuario.addAll(listaAtraccionesGeneral);
-				listaPromocionesParaUsuario = new ArrayList<Promocion>();
-				listaPromocionesParaUsuario.addAll(listaPromocionesGeneral);
+			try {
+				for (Usuario usuario : listaUsuarios) {
+					// Se clonan para no referenciar al original y no borrar elementos del original
+					listaAtraccionesUsuario = new ArrayList<Atraccion>();
+					listaAtraccionesUsuario.addAll(listaAtraccionesGeneral);
+					listaPromocionesParaUsuario = new ArrayList<Promocion>();
+					listaPromocionesParaUsuario.addAll(listaPromocionesGeneral);
 
-				// Se generan los listados de opciones filtrados
-				atraccionesParaUsuario = generador.armarPosiblesAtraccionesParaUsuario(usuario, listaAtraccionesUsuario);
-				promocionesParaUsuario = generador.armarPosiblesPromocionesParaUsuario(usuario, listaPromocionesParaUsuario);
+					// Se generan los listados de opciones filtrados
+					atraccionesParaUsuario = generador.armarPosiblesAtraccionesParaUsuario(usuario,
+							listaAtraccionesUsuario);
+					promocionesParaUsuario = generador.armarPosiblesPromocionesParaUsuario(usuario,
+							listaPromocionesParaUsuario);
 
-					while(atraccionesParaUsuario.size() > 0 || promocionesParaUsuario.size() > 0) {
+					while (atraccionesParaUsuario.size() > 0 || promocionesParaUsuario.size() > 0) {
 						opcionSeleccionada = 0;
 						view.mostrarMenu(usuario, atraccionesParaUsuario, promocionesParaUsuario);
-						
-						opcionSeleccionada = Integer.parseInt(in.readLine())-1;
-											
-						int totalItems = listaPromocionesParaUsuario.size()+listaAtraccionesUsuario.size();
-						
-						if (opcionSeleccionada > -1 && totalItems > opcionSeleccionada) {	
-														
-							if(opcionSeleccionada < promocionesParaUsuario.size()) {
+
+						opcionSeleccionada = Integer.parseInt(in.readLine()) - 1;
+
+						int totalItems = listaPromocionesParaUsuario.size() + listaAtraccionesUsuario.size();
+
+						if (opcionSeleccionada > -1 && totalItems > opcionSeleccionada) {
+
+							if (opcionSeleccionada < promocionesParaUsuario.size()) {
 								// Se selecciono Promocion
 								Promocion promocionSeleccionada = promocionesParaUsuario.get(opcionSeleccionada);
 								view.log("Se selecciono la promocion: " + promocionSeleccionada.getNombre());
-								
+
 								try {
 									usuario.addPromocion(promocionSeleccionada);
-									usuario.setCantidadDeMonedas(usuario.getCantidadDeMonedas() - promocionSeleccionada.getCosto());
-									usuario.setTiempoDisponible(usuario.getTiempoDisponible() - promocionSeleccionada.getTiempoDeDuracion());
-									
-									for(Atraccion atr : promocionSeleccionada.getAtracciones()) {
-										atr.setcapacidadRestante(atr.getcapacidadRestante()-1);
+									usuario.setCantidadDeMonedas(
+											usuario.getCantidadDeMonedas() - promocionSeleccionada.getCosto());
+									usuario.setTiempoDisponible(usuario.getTiempoDisponible()
+											- promocionSeleccionada.getTiempoDeDuracion());
+
+									for (Atraccion atr : promocionSeleccionada.getAtracciones()) {
+										atr.setcapacidadRestante(atr.getcapacidadRestante() - 1);
 									}
-								}
-								catch(Exception e) {
+								} catch (Exception e) {
 									view.log("Error al agregar promocion: " + e.getMessage());
 								}
-							}
-							else if ((opcionSeleccionada-promocionesParaUsuario.size()) < atraccionesParaUsuario.size()){
+							} else if ((opcionSeleccionada - promocionesParaUsuario.size()) < atraccionesParaUsuario
+									.size()) {
 								opcionSeleccionada -= promocionesParaUsuario.size();
-								view.log("Se selecciono la atraccion: " + atraccionesParaUsuario.get(opcionSeleccionada).getNombre());
-								
+								view.log("Se selecciono la atraccion: "
+										+ atraccionesParaUsuario.get(opcionSeleccionada).getNombre());
+
 								try {
 									usuario.addAtraccion(atraccionesParaUsuario.get(opcionSeleccionada));
-									usuario.setCantidadDeMonedas(usuario.getCantidadDeMonedas() - atraccionesParaUsuario.get(opcionSeleccionada).getCosto());
-									usuario.setTiempoDisponible(usuario.getTiempoDisponible() - atraccionesParaUsuario.get(opcionSeleccionada).getTiempoDeDuracion());
-									atraccionesParaUsuario.get(opcionSeleccionada).setcapacidadRestante(atraccionesParaUsuario.get(opcionSeleccionada).getcapacidadRestante()-1);
-								}
-								catch(Exception e) {
+									usuario.setCantidadDeMonedas(usuario.getCantidadDeMonedas()
+											- atraccionesParaUsuario.get(opcionSeleccionada).getCosto());
+									usuario.setTiempoDisponible(usuario.getTiempoDisponible()
+											- atraccionesParaUsuario.get(opcionSeleccionada).getTiempoDeDuracion());
+									atraccionesParaUsuario.get(opcionSeleccionada).setcapacidadRestante(
+											atraccionesParaUsuario.get(opcionSeleccionada).getcapacidadRestante() - 1);
+								} catch (Exception e) {
 									System.out.println("Error: " + e.getMessage());
 								}
-							}  else {
+							} else {
 								view.log("Numero invalido 2");
 							}
-						}
-						else {
+						} else {
 							view.log("Numero invalido");
 						}
 						// Se refrescan las listas de posibles opciones
-						atraccionesParaUsuario = generador.armarPosiblesAtraccionesParaUsuario(usuario, atraccionesParaUsuario);
-						promocionesParaUsuario = generador.armarPosiblesPromocionesParaUsuario(usuario, listaPromocionesParaUsuario);
-						
+						atraccionesParaUsuario = generador.armarPosiblesAtraccionesParaUsuario(usuario,
+								atraccionesParaUsuario);
+						promocionesParaUsuario = generador.armarPosiblesPromocionesParaUsuario(usuario,
+								listaPromocionesParaUsuario);
+
 					}
-			};
-			archivo.generarItinerarios(listaUsuarios);
-		}
-		
-		catch(Exception e) {
-			System.err.println("Error " + e);
-		}
-		
-		}
-		else {
+				}
+				;
+				archivo.generarItinerarios(listaUsuarios);
+			}
+
+			catch (Exception e) {
+				System.err.println("Error " + e.getMessage());
+			}
+
+		} else {
 			view.log("Listas vacias");
 		}
-		
+
 	}
-	
+
 	private void leerArchivos() {
 		try {
 			listaUsuarios = archivo.LeerUsuarios("usuarios");
 			listaAtraccionesGeneral = archivo.LeerAtracciones("atracciones");
 			listaPromocionesGeneral = archivo.LeerPromociones("promociones", listaAtraccionesGeneral);
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	
 
-	
-
-	
 }
--- TierraMedia/src/utilidades/Archivo.java
+++ TierraMedia/src/utilidades/Archivo.java
@@ -1,4 +1,5 @@
 package utilidades;
+
 import java.io.*;
 import java.util.*;
 
@@ -9,241 +10,219 @@ import promociones.AxB;
 import promociones.Porcentual;
 
 public class Archivo {
-	
+
 	final String SEPARADOR = ",";
 	File archivo = null;
 	BufferedReader bufferReader = null;
-    String linea = "";
-    FileWriter fileWriter = null;
+	String linea = "";
+	FileWriter fileWriter = null;
 	BufferedWriter bufferWriter = null;
 	Visualizador view = new Visualizador();
-    	
+
 	// Lectura de Archivos
-	
+
 	public List<Usuario> LeerUsuarios(String archivo) throws Exception {
 
 		List<Usuario> listaUsuarios = new ArrayList<Usuario>();
 		bufferReader = getBufferReader(archivo);
-			
+
 		try {
 			while ((linea = bufferReader.readLine()) != null) {
 				String[] datos = linea.split(SEPARADOR);
 				listaUsuarios.add(crearUsuario(datos));
-				
+
 			}
-			
+
 			return listaUsuarios;
 		} catch (IOException e) {
 			throw new ArmarListaException(archivo);
 		} finally {
 			cerrarBufferedReader(bufferReader, archivo);
 		}
-		
+
 	}
-	
+
 	public List<Atraccion> LeerAtracciones(String archivo) throws Exception {
-			
-			List<Atraccion> listaAtracciones = new ArrayList<Atraccion>();
-			bufferReader = getBufferReader(archivo);
-				
-			try {			
-				while ((linea = bufferReader.readLine()) != null) {
-					String[] datos = linea.split(SEPARADOR);
-					listaAtracciones.add(crearAtraccion(datos));
-					
-				}
-				
-				return listaAtracciones;
-			} catch (IOException e) {
-				throw new ArmarListaException(archivo);
-			} finally {
-				cerrarBufferedReader(bufferReader, archivo);
+
+		List<Atraccion> listaAtracciones = new ArrayList<Atraccion>();
+		bufferReader = getBufferReader(archivo);
+
+		try {
+			while ((linea = bufferReader.readLine()) != null) {
+				String[] datos = linea.split(SEPARADOR);
+				listaAtracciones.add(crearAtraccion(datos));
+
 			}
-		
-			
+
+			return listaAtracciones;
+		} catch (IOException e) {
+			throw new ArmarListaException(archivo);
+		} finally {
+			cerrarBufferedReader(bufferReader, archivo);
 		}
-	
+
+	}
+
 	public List<Promocion> LeerPromociones(String archivo, List<Atraccion> listaAtracciones) throws Exception {
-		
+
 		List<Promocion> listaPromociones = new ArrayList<Promocion>();
 		List<Atraccion> atracciones = null;
 		bufferReader = getBufferReader(archivo);
-	
+
 		try {
 			while ((linea = bufferReader.readLine()) != null) {
 				String[] datos = linea.split(";");
-				
+
 				atracciones = new ArrayList<Atraccion>();
-	
-				for(String atr : datos[1].split(SEPARADOR)) {
-					atracciones.add(listaAtracciones.get(obtenerIndice(atr, listaAtracciones)-1));
-				}	
-				
-				listaPromociones.add(crearPromocion(datos[0],atracciones, datos[2], datos[3], datos[4]));
-				
+
+				for (String atr : datos[1].split(SEPARADOR)) {
+					atracciones.add(listaAtracciones.get(obtenerIndice(atr, listaAtracciones) - 1));
+				}
+
+				listaPromociones.add(crearPromocion(datos[0], atracciones, datos[2], datos[3], datos[4]));
+
 			}
-			
+
 			return listaPromociones;
 		} catch (IOException e) {
 			throw new ArmarListaException(archivo);
 		} finally {
 			cerrarBufferedReader(bufferReader, archivo);
 		}
-		
+
 	}
-	
-	// Crear 
-	
+
+	// Crear
+
 	private Atraccion crearAtraccion(String[] datos) {
-		  
-		  switch(datos[4]) {
-			 case "Aventura":
-				 return new Atraccion(datos[0],
-				    		Integer.parseInt(datos[1]),
-				    		Double.parseDouble(datos[2]),
-				    		Integer.parseInt(datos[3]),
-				    		TipoDeAtracciones.Aventura);
-			 case "Degustacion":
-				 return new Atraccion(datos[0],
-				    		Integer.parseInt(datos[1]),
-				    		Double.parseDouble(datos[2]),
-				    		Integer.parseInt(datos[3]),
-				    		TipoDeAtracciones.Desgustacion);
-			 default:
-				 return new Atraccion(datos[0],
-				    		Integer.parseInt(datos[1]),
-				    		Double.parseDouble(datos[2]),
-				    		Integer.parseInt(datos[3]),
-				    		TipoDeAtracciones.Paisaje);
-		 }
-		  
-	  }
-	  
+
+		switch (datos[4]) {
+		case "Aventura":
+			return new Atraccion(datos[0], Integer.parseInt(datos[1]), Double.parseDouble(datos[2]),
+					Integer.parseInt(datos[3]), TipoDeAtracciones.Aventura);
+		case "Degustacion":
+			return new Atraccion(datos[0], Integer.parseInt(datos[1]), Double.parseDouble(datos[2]),
+					Integer.parseInt(datos[3]), TipoDeAtracciones.Desgustacion);
+		default:
+			return new Atraccion(datos[0], Integer.parseInt(datos[1]), Double.parseDouble(datos[2]),
+					Integer.parseInt(datos[3]), TipoDeAtracciones.Paisaje);
+		}
+
+	}
+
 	private Usuario crearUsuario(String[] datos) {
-		  switch(datos[3]) {
-			 case "Aventura":
-				 return new Usuario(
-							datos[0],
-							Integer.parseInt(datos[1]),
-							Double.parseDouble(datos[2]),
-							TipoDeAtracciones.Aventura
-						);
-			 case "Degustacion":
-				 return new Usuario(
-							datos[0],
-							Integer.parseInt(datos[1]),
-							Double.parseDouble(datos[2]),
-							TipoDeAtracciones.Desgustacion
-						);
-			 default:
-				 return new Usuario(
-							datos[0],
-							Integer.parseInt(datos[1]),
-							Double.parseDouble(datos[2]),
-							TipoDeAtracciones.Paisaje
-						);
-		 }
-		  
-	  }
-	
+		switch (datos[3]) {
+		case "Aventura":
+			return new Usuario(datos[0], Integer.parseInt(datos[1]), Double.parseDouble(datos[2]),
+					TipoDeAtracciones.Aventura);
+		case "Degustacion":
+			return new Usuario(datos[0], Integer.parseInt(datos[1]), Double.parseDouble(datos[2]),
+					TipoDeAtracciones.Desgustacion);
+		default:
+			return new Usuario(datos[0], Integer.parseInt(datos[1]), Double.parseDouble(datos[2]),
+					TipoDeAtracciones.Paisaje);
+		}
+
+	}
+
 	private <T> Promocion crearPromocion(String nombre, List<Atraccion> atracciones, String tipo, T datoExtra,
 			String descripcion) {
 
 		switch (tipo) {
 		case "Porcentual":
-			return new Porcentual(nombre, atracciones, descripcion, (int) datoExtra);
+			return new Porcentual(nombre, atracciones, Integer.parseInt((String) datoExtra), descripcion);
 		case "Absoluta":
-			return new Absoluta(nombre, (int) datoExtra, atracciones, descripcion);
+			return new Absoluta(nombre, atracciones, Integer.parseInt((String) datoExtra), descripcion);
 		case "AxB":
-			return new AxB(nombre, atracciones, (String) datoExtra);
+			return new AxB(nombre, atracciones, (String) datoExtra, descripcion);
 		default:
 			return null;
 		}
 	}
-		
+
 	// Utils
-	
-	public  String RutaActual () {
-		  	String ruta = "";
-		     File directorio = new File (".");
-		     try {
-		    	 ruta = directorio.getCanonicalPath();
-		       }catch(Exception e) {
-		    	   e.printStackTrace();
-		       }
-			return ruta;
-		  }
-		  
-	public void guardar( List<String> datos ) throws Exception{
-		  
-			String ruta = RutaActual()+"/src/archivos/itinerario.txt";
-			view.log("Guardando... ");
-			try{	
-				fileWriter = new FileWriter(ruta, false);
-				bufferWriter =  new BufferedWriter(fileWriter);
-			
-				for(String dato : datos) {
-					bufferWriter.write(dato);					
-				}
-			}catch(Exception e) {
-				throw new EscribirArchivoException("itinerario");
-		    } finally {
-	            try {
-	                if (bufferWriter != null)
-	                    bufferWriter.close();
-	                if(fileWriter != null)
-	                	fileWriter.close();
-	            } catch (IOException ex) {
-	                System.err.println("Error al cerrar BufferWriter y/o FileWriter en metodo guardar");
-	            }
-	        }
-	  }
-	  
+
+	public String RutaActual() {
+		String ruta = "";
+		File directorio = new File(".");
+		try {
+			ruta = directorio.getCanonicalPath();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ruta;
+	}
+
+	public void guardar(List<String> datos) throws Exception {
+
+		String ruta = RutaActual() + "/src/archivos/itinerario.txt";
+		view.log("Guardando... ");
+		try {
+			fileWriter = new FileWriter(ruta, false);
+			bufferWriter = new BufferedWriter(fileWriter);
+
+			for (String dato : datos) {
+				bufferWriter.write(dato);
+			}
+		} catch (Exception e) {
+			throw new EscribirArchivoException("itinerario");
+		} finally {
+			try {
+				if (bufferWriter != null)
+					bufferWriter.close();
+				if (fileWriter != null)
+					fileWriter.close();
+			} catch (IOException ex) {
+				System.err.println("Error al cerrar BufferWriter y/o FileWriter en metodo guardar");
+			}
+		}
+	}
+
 	public void generarItinerarios(List<Usuario> listaUsuarios) throws Exception {
-		  view.log("Generando...");
-		  List<String> itinerarios = new ArrayList<String>();
-		  String linea = "";
-		  for(Usuario user : listaUsuarios) {
-			  	linea = "";
-				linea += "----------------------------------------------------------\n";
-				linea += "Itinerario para: " + user.getNombre() + "\n";
-				linea += view.generarItinerarioPorUsuario(user);
-				itinerarios.add(linea);		
-		  }
-		  
-		  guardar(itinerarios);
-		  
-	  }
-	
+		view.log("Generando...");
+		List<String> itinerarios = new ArrayList<String>();
+		String linea = "";
+		for (Usuario user : listaUsuarios) {
+			linea = "";
+			linea += "----------------------------------------------------------\n";
+			linea += "Itinerario para: " + user.getNombre() + "\n";
+			linea += view.generarItinerarioPorUsuario(user);
+			itinerarios.add(linea);
+		}
+
+		guardar(itinerarios);
+
+	}
+
 	private int obtenerIndice(String dato, List<Atraccion> lista) {
-		
+
 		ListIterator<Atraccion> itr = lista.listIterator();
 
-		while(itr.hasNext()) {
-			if(itr.next().getNombre().equals(dato))
+		while (itr.hasNext()) {
+			if (itr.next().getNombre().equals(dato))
 				return itr.nextIndex();
-		};
-		
+		}
+		;
+
 		return -1;
-		
+
 	}
 
 	// BufferedReader Utils
-	
+
 	private BufferedReader getBufferReader(String nombreArchivo) throws Exception {
-		String ruta = RutaActual()+"/src/archivos/"+nombreArchivo.toLowerCase()+".txt";
+		String ruta = RutaActual() + "/src/archivos/" + nombreArchivo.toLowerCase() + ".txt";
 		try {
-			File archivo = new File (ruta);
-			FileReader fileReader = new FileReader (archivo);
+			File archivo = new File(ruta);
+			FileReader fileReader = new FileReader(archivo);
 			return new BufferedReader(fileReader);
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			throw new LecturaArchivoException(nombreArchivo);
 		}
-		
+
 	}
-	
-	private void cerrarBufferedReader(BufferedReader buffered, String archivo) throws Exception{
+
+	private void cerrarBufferedReader(BufferedReader buffered, String archivo) throws Exception {
 		try {
 			if (bufferReader != null)
 				bufferReader.close();
